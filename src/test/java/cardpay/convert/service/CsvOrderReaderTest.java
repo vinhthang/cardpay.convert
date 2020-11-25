45736bf968d22fe9252e283b5e38ed0113bf744b
Thang Hoang <thang.hoang@evizi.com>
2020-11-26 00:07:41 +0700

    add test code for csv reader, json reader

--- src/test/java/cardpay/convert/service/CsvOrderReaderTest.java
+++ src/test/java/cardpay/convert/service/CsvOrderReaderTest.java
@@ -18,32 +18,25 @@ public class CsvOrderReaderTest {
     @Test
     public void testReadLine_success() {
         Order order = csvOrderReader.parseLine("1,100,USD,order payment");
-        log.debug("order", order);
-        assertThat(order != null);
-        assertThat(order.getId() == 1);
-        assertThat(order.getAmount() == 100.0);
-        assertThat("OK".equals(order.getResult()));
+        assertThat(order).isNotNull();
+        assertThat(order.getId()).isEqualTo(1);
+        assertThat(order.getAmount()).isEqualTo(100.0);
+        assertThat(order.getResult()).isEqualTo("OK");
     }
 
     @Test
     public void testReadLine_wrong_amount() {
         Order order = csvOrderReader.parseLine("1,10x0,USD,order payment");
-        log.debug("order", order);
-        assertThat(order != null);
-        assertThat(order.getId() == 1);
-        assertThat(order.getAmount() == null);
-        assertThat(!"OK".equals(order.getResult()));
-        assertThat("Amount 10x0".equals(order.getResult()));
+        assertThat(order).isNotNull();
+        assertThat(order.getId()).isEqualTo(1);
+        assertThat(order.getAmount()).isNull();
+        assertThat(order.getResult()).isNotEqualTo("OK");
     }
 
     @Test
     public void testReadLine_wrong_format() {
         Order order = csvOrderReader.parseLine("1,,10x0,USD,order payment");
-        log.debug("order", order);
-        assertThat(order != null);
-        assertThat(order.getId() == 1);
-        assertThat(order.getAmount() == null);
-        assertThat(!"OK".equals(order.getResult()));
-        assertThat("Wrong format: 1,,10x0,USD,order payment".equals(order.getResult()));
+        assertThat(order).isNotNull();
+        assertThat(order.getResult()).isEqualTo("Wrong format: 1,,10x0,USD,order payment");
     }
 }
---
+++ src/test/java/cardpay/convert/service/JsonOrderReaderTest.java
@@ -0,0 +1,44 @@
+package cardpay.convert.service;
+
+
+import cardpay.convert.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+public class JsonOrderReaderTest {
+    @Autowired
+    private OrderReader jsonOrderReader;
+
+    @Test
+    public void testReadLine_success() {
+        Order order = jsonOrderReader.parseLine("{\"orderId\":3,\"amount\":1.23,\"currency\":\"USD\",\"comment\":\"order payment\"}");
+        log.debug("order", order);
+        assertThat(order != null);
+        assertThat(order.getId() == 1);
+        assertThat(order.getAmount() == 100.0);
+        assertThat("OK".equals(order.getResult()));
+    }
+
+    @Test
+    public void testReadLine_wrong_amount() {
+        Order order = jsonOrderReader.parseLine("{\"orderId\":3,\"amount\":1.x23,\"currency\":\"USD\",\"comment\":\"order payment\"}");
+        log.debug("order", order);
+        assertThat(order != null);
+        assertThat(!"OK".equals(order.getResult()));
+    }
+
+    @Test
+    public void testReadLine_wrong_format() {
+        String line = "{\"orderId\":3x,\"amount\":1.23,\"currency\":\"USD\",\"comment\":\"order payment\"}";
+        Order order = jsonOrderReader.parseLine(line);
+        log.debug("order", order);
+        assertThat(order != null);
+        assertThat(order.getResult()).isEqualTo("Wrong format: " + line);
+    }
+}
